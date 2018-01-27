faab0ae4ed790a76a6301ec433770884a4233538
Wilson Blaikie <wilsonblaikie@gmail.com>
2018-01-27 01:31:46 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,6 +7,11 @@ def display_board(board)
 end
 
 def input_to_index(input)
-  index = input.to_i
-  return index
+  index = (input.to_i) - 1
+  index.is_a? Integer
+    return index
+  else
+    return -1
+  end
+  
 end
